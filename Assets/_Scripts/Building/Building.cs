084143a7f35226b04edee650e8d35bca1c897a25
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-21 22:49:15 +0100

    Removed LineRendererHighlight.cs

--- Assets/_Scripts/Building/Building.cs
+++ Assets/_Scripts/Building/Building.cs
@@ -1,7 +1,36 @@
-﻿namespace Buildings
-{
-    public interface IBuilding
-    {
-        public void Demolish();
-    }
+
+using UnityEngine;
+
+namespace Buildings
+{ 
+	public abstract class Building : MonoBehaviour
+	{
+		#region Inspector Variables
+		
+		#endregion
+	
+		#region Public Variables
+		
+		#endregion
+
+		#region Private Variables
+		
+		#endregion
+
+		#region Unity Methods
+		
+		#endregion
+
+		#region Public Methods
+
+		public abstract void Demolish();
+
+		#endregion
+
+		#region Private Methods
+
+		#endregion
+
+
+	}
 }
