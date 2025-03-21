f8e81ec7fee51fe7f464c5ebcde2aeb1101c0a96
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-21 22:52:59 +0100

    Added RoadFunctionality.cs

---
+++ Assets/_Scripts/Building/RoadFunctionality.cs
@@ -0,0 +1,42 @@
+
+using Managers;
+
+namespace Buildings
+{ 
+	public class RoadFunctionality : Building
+	{
+		#region Inspector Variables
+		
+		#endregion
+	
+		#region Public Variables
+		
+		#endregion
+
+		#region Private Variables
+		
+		#endregion
+
+		#region Unity Methods
+		
+		#endregion
+
+		#region Public Methods
+		
+		public override void Demolish()
+		{
+			ResourcesManager.Instance.AddGold((int)(BuildManager.Instance.RoadPrice * 0.8));
+			MapManager.Instance.DemolishRoad(transform.parent.transform.position);
+			UIManagerInGame.Instance.DisableAllHUDExceptBuildPanel();
+			Destroy(gameObject);
+		}
+		
+		#endregion
+
+		#region Private Methods
+		
+		#endregion
+
+
+	}
+}
