0f8f3f9be18fc4f6961a03a07665c0c6d4bdeecd
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-24 19:34:39 +0100

    Added OnDemolish event to HouseFunctionality.cs

--- Assets/_Scripts/Building/HouseFunctionality.cs
+++ Assets/_Scripts/Building/HouseFunctionality.cs
@@ -2,7 +2,6 @@ using System;
 using Residents;
 using Managers;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Buildings
 {
@@ -11,27 +10,20 @@ namespace Buildings
         #region Inspector Variables
 
         [SerializeField] private Light[] lights;
-        [Range(1, 50)] [SerializeField] private int minSecondsBetweenHabitantes;
-        [Range(2, 100)] [SerializeField] private int maxSecondsBetweenHabitantes;
 
         #endregion
 
         #region Public Variables
 
         public int Inhabitants { get; private set; }
-
         public int Level { get; private set; }
-
         public int CurrentGoldPerSecond { get; private set; }
-
         public int CostsPerSecond { get; private set; }
-
         public float Multiplier { get; private set; }
-
         public int MaxInhabitants { get; private set; }
-
         public int LevelPrice { get; private set; }
 
+        public event Action OnDemolish;
         public event Action OnResidentUpdate;
         public event Action OnLevelUpdate;
 
@@ -41,8 +33,6 @@ namespace Buildings
 
         private int _currentGoldInHand;
         private int _currentLight;
-        private int _secondsBetweenSpawns;
-        private float _timerToSpawnHabitante;
 
         #endregion
 
@@ -50,7 +40,6 @@ namespace Buildings
 
         private void Awake()
         {
-            _timerToSpawnHabitante = Random.Range(1.5f, 3.2f);
             _currentLight = 0;
             Inhabitants = 0;
             Level = 1;
@@ -59,58 +48,38 @@ namespace Buildings
             MaxInhabitants = 1;
             LevelPrice = (int)(BuildManager.Instance.HousePrice * 2.5f);
             BuildingsManager.Instance.AddHouse();
+            InhabitantsManager.AddAvailableHouse(this);
         }
 
         private void Update()
         {
-            //Each second genere gold
+            //Each second generate gold
             if (Time.frameCount % Application.targetFrameRate == 0)
             {
                 GenerateGold();
-            }
-
-            if (MaxInhabitants > Inhabitants) // Is there space for a new resident?
-            {
-                GenerateResident();
             }
         }
 
         #endregion
 
         #region Public Methods
-
-        public void AddNewResident(HabitanteStat h)
-        {
-            AudioManager.Instance.PlaySFXSound(AudioManager.SFX_Type.newResident);
-            if (Inhabitants > 0)
-                ResourcesManager.Instance.AddGoldPerSecond(-CurrentGoldPerSecond * Multiplier);
-            Inhabitants++;
-            CurrentGoldPerSecond += h.BaseGold;
-            Multiplier += h.MultiplicadorGold;
-            ResourcesManager.Instance.AddGoldPerSecond(CurrentGoldPerSecond * Multiplier);
-            CostsPerSecond += h.CostsPerSecond;
-            UpdateLight(h.EmissionColor);
-            //BuildingsManager.Instance.RemoveCasa(transform.parent.gameObject);
-            ResourcesManager.Instance.AddResident(1);
-            ResourcesManager.Instance.AddCosts(h.CostsPerSecond);
-            OnResidentUpdate?.Invoke();
-        }
-
+        
         public void LevelUp()
         {
-            if (ResourcesManager.Instance.CurrentGold > LevelPrice)
+            if (ResourcesManager.CurrentGold > LevelPrice)
             {
                 AudioManager.Instance.PlaySFXSound(AudioManager.SFX_Type.levelUp);
-                ResourcesManager.Instance.AddGold(-LevelPrice);
+                ResourcesManager.AddGold(-LevelPrice);
                 Level++;
                 MaxInhabitants++;
                 LevelPrice *= 2;
                 if (Inhabitants > 0)
-                    ResourcesManager.Instance.AddGoldPerSecond((-CurrentGoldPerSecond * Multiplier));
+                    ResourcesManager.AddGoldPerSecond((-CurrentGoldPerSecond * Multiplier));
                 Multiplier += 0.1f;
-                ResourcesManager.Instance.AddGoldPerSecond((CurrentGoldPerSecond * Multiplier));
-                ResourcesManager.Instance.AddCosts((int)(CostsPerSecond * 0.2f));
+                ResourcesManager.AddGoldPerSecond((CurrentGoldPerSecond * Multiplier));
+                ResourcesManager.AddCosts((int)(CostsPerSecond * 0.2f));
                 CostsPerSecond = (int)(CostsPerSecond * 1.2f);
+                InhabitantsManager.AddAvailableHouse(this);
                 OnLevelUpdate?.Invoke();
             }
             else
@@ -122,9 +91,9 @@ namespace Buildings
         public void UpgradeMultiplier(float m)
         {
             if (Inhabitants > 0)
-                ResourcesManager.Instance.AddGoldPerSecond((-CurrentGoldPerSecond * Multiplier));
+                ResourcesManager.AddGoldPerSecond((-CurrentGoldPerSecond * Multiplier));
             Multiplier += m;
-            ResourcesManager.Instance.AddGoldPerSecond((CurrentGoldPerSecond * Multiplier));
+            ResourcesManager.AddGoldPerSecond((CurrentGoldPerSecond * Multiplier));
         }
 
         public override string ToString()
@@ -135,38 +104,42 @@ namespace Buildings
 
         public override void Demolish()
         {
-            ResourcesManager.Instance.AddGold((int)(BuildManager.Instance.HousePrice * 0.8f * Level));
-            ResourcesManager.Instance.AddGoldPerSecond((-CurrentGoldPerSecond * Multiplier));
-            ResourcesManager.Instance.AddCosts(-CostsPerSecond);
-            ResourcesManager.Instance.AddResident(-Inhabitants);
+            ResourcesManager.AddGold((int)(BuildManager.Instance.HousePrice * 0.8f * Level));
+            ResourcesManager.AddGoldPerSecond((-CurrentGoldPerSecond * Multiplier));
+            ResourcesManager.AddCosts(-CostsPerSecond);
+            ResourcesManager.AddResident(-Inhabitants);
             BuildingsManager.Instance.RemoveCasa(MapManager.Instance.GetTile(transform.parent.position));
             UIManagerInGame.Instance.DisableAllHUDExceptBuildPanel();
+            OnDemolish?.Invoke();
             Destroy(gameObject);
         }
-
+        
         #endregion
 
         #region Private Methods
-
-        private void GenerateResident()
+        
+        private void AddNewResident(ResidentStat h)
         {
-            if (_timerToSpawnHabitante < 0)
-            {
-                InhabitantsManager.Instance.SummonHabitante(transform.gameObject);
-                _secondsBetweenSpawns = Random.Range(minSecondsBetweenHabitantes, maxSecondsBetweenHabitantes);
-                _timerToSpawnHabitante = _secondsBetweenSpawns;
-            }
-            else
-            {
-                _timerToSpawnHabitante -= Time.deltaTime;
-            }
+            AudioManager.Instance.PlaySFXSound(AudioManager.SFX_Type.newResident);
+            if (Inhabitants > 0)
+                ResourcesManager.AddGoldPerSecond(-CurrentGoldPerSecond * Multiplier);
+            Inhabitants++;
+            CurrentGoldPerSecond += h.BaseGold;
+            Multiplier += h.MultiplicadorGold;
+            ResourcesManager.AddGoldPerSecond(CurrentGoldPerSecond * Multiplier);
+            CostsPerSecond += h.CostsPerSecond;
+            UpdateLight(h.EmissionColor);
+            //BuildingsManager.Instance.RemoveCasa(transform.parent.gameObject);
+            ResourcesManager.AddResident(1);
+            ResourcesManager.AddCosts(h.CostsPerSecond);
+            OnResidentUpdate?.Invoke();
         }
-
+        
         private void GenerateGold()
         {
             if (Inhabitants > 0)
             {
-                ResourcesManager.Instance.AddGold((int)(CurrentGoldPerSecond * Multiplier));
+                ResourcesManager.AddGold((int)(CurrentGoldPerSecond * Multiplier));
             }
         }
 
@@ -177,12 +150,17 @@ namespace Buildings
             l.color = c;
             l.enabled = true;
         }
+        
+        private bool IsThereSpaceForResident()
+        {
+            return MaxInhabitants > Inhabitants;
+        }
 
         private void OnTriggerEnter(Collider other)
         {
-            if (!other.CompareTag("Habitante")) return;
+            if (!other.CompareTag("Resident")) return;
             if (MaxInhabitants > Inhabitants)
-                AddNewResident(other.GetComponent<HabitanteStat>()); //If there is room for residente
+                AddNewResident(other.GetComponent<ResidentStat>()); //If there is room for residente
             Destroy(other.gameObject);
         }
 
