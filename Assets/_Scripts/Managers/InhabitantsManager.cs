5e7e58c11d8204b9fb92f958df962bd8e4b93039
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-24 19:35:46 +0100

    Added spawn of inhabitants to InhabitantsManager instead of houseFunctionality

--- Assets/_Scripts/Managers/InhabitantsManager.cs
+++ Assets/_Scripts/Managers/InhabitantsManager.cs
@@ -1,16 +1,21 @@
+using System;
 using System.Collections.Generic;
+using Buildings;
 using Residents;
 using Utilities;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace Managers
 {
-    public class InhabitantsManager : MonoBehaviourSinglenton<InhabitantsManager>
+    public class InhabitantsManager : MonoBehaviourSingleton<InhabitantsManager>
     {
         #region Inspector Variables
 
-        [SerializeField] private List<GameObject> Prefabs;
+        [SerializeField] private List<ResidentIA> residentPrefabs;
+        [Range(1, 5)] [SerializeField] private float minSecondsBetweenResidents = 1f;
+        [Range(2, 10)] [SerializeField] private float maxSecondsBetweenResidents = 3f;
 
         #endregion
 
@@ -26,26 +31,68 @@ namespace Managers
         #region Private Variables
 
         private Vector3 _roadPosition; //SpawnPoint
-
+        private static readonly Queue<HouseFunctionality> AvailableHouses = new();
+        private float _timerToSpawnResident;
+        
+        
         #endregion
 
-        #region Unity Methods (EMPTY)
+        #region Unity Methods
+
+        private void Awake()
+        {
+            _timerToSpawnResident = Random.Range(minSecondsBetweenResidents, maxSecondsBetweenResidents);
+        }
+
+        private void Update()
+        {
+            GenerateResident();
+        }
 
         #endregion
 
         #region Public Methods
 
-        public void SummonHabitante(GameObject casa)
+        public static void AddAvailableHouse(HouseFunctionality houseFunctionality)
         {
-            GameObject habitante = Instantiate(Prefabs[Random.Range(0, 3)], _roadPosition + (Vector3.up * 15f),
-                Quaternion.identity);
-            habitante.GetComponent<HabitanteIA>().BuildPath(casa);
-        }
-
+            AvailableHouses.Enqueue(houseFunctionality);
+        } 
+        
         #endregion
 
-        #region Private Methods (EMPTY)
+        #region Private Methods
+        
+        private void GenerateResident()
+        {
+            if (AvailableHouses.Count == 0) return;
+            if (_timerToSpawnResident < 0)
+            {
+                var timeToNextResident = Random.Range(minSecondsBetweenResidents, maxSecondsBetweenResidents);
+                // Debug.Log($"New resident generated. Time to next: {timeToNextResident}");
+                _timerToSpawnResident = timeToNextResident;
+                if(GetNextAvailableHouse(out var house)) Instance.SummonResident(house);
+            }
 
+            _timerToSpawnResident -= Time.deltaTime;
+        }
+
+        private static bool GetNextAvailableHouse(out HouseFunctionality house)
+        {
+            house = AvailableHouses.Dequeue();
+            while (house.IsNull() && AvailableHouses.Count > 0)
+            {
+                house = AvailableHouses.Dequeue();
+            }
+
+            return house;
+        }
+        
+        private void SummonResident(HouseFunctionality house)
+        {
+            var resident = Instantiate(residentPrefabs[Random.Range(0, 3)], _roadPosition + (Vector3.up * 15f), Quaternion.identity);
+            resident.BuildPath(house);
+        }
+        
         #endregion
     }
 }
